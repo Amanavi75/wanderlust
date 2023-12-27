3a4211dcd0fcdb08e06478dbb0ed7373801b33ac
amanavi75 <amanavi75@gmail.com>
2023-12-27 11:57:37 +0530

    connected with the database

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const app = express();
 const mongoose = require("mongoose")
 
 
+const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust"
+// database connection 
+
+main()
+.then(()=>{
+    console.log("connected to db ")
+}).catch(err=>{
+    console.log(err);
+})
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+
 app.get('/',(req,res)=>{
     res.send("hi i am root ");
 })
