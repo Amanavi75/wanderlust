ee092e20832332629d32def2a38552d7c3201823
amanavi75 <amanavi75@gmail.com>
2023-12-27 12:22:57 +0530

    created listing model and tested with our database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express();
 const mongoose = require("mongoose")
+const Listing = require("./models/listing.js")
 
 
 const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust"
@@ -20,6 +21,25 @@ async function main(){
 app.get('/',(req,res)=>{
     res.send("hi i am root ");
 })
+app.get("/testListing",async (req,res)=>{
+    let sampleListing = new Listing ({
+        title:"my new vila",
+        description:"by the beach",
+        price:1200,
+        location:"calangute",
+        country:"India"
+    });
+
+    await sampleListing.save();
+    console.log("sample was save")
+    res.send("successfull listing")
+})
+
+
+
+
+
 app.listen(8080,()=>{
     console.log("server is listening to port 8080");
 })
+
