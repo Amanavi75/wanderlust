1a13943ecc017e8c1c6335590a21ead68a9c8095
amanavi75 <amanavi75@gmail.com>
2023-12-28 11:43:48 +0530

    created showe.js and index.ejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express();
 const mongoose = require("mongoose")
 const Listing = require("./models/listing.js")
+const path = require("path")
 
 
 const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust"
@@ -17,11 +18,32 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}))
+// it will parse all the data that is coming inside the request 
+
 
 app.get('/',(req,res)=>{
     res.send("hi i am root ");
 })
 
+
+// index route 
+//* it will show all the listings as the list
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  });
+
+
+  //Show Route 
+  app.get("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id)
+    res.render("listings/show.ejs", {listing});
+
+  })
 
 /*app.get("/testListing",async (req,res)=>{
     let sampleListing = new Listing ({
