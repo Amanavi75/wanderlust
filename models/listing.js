9288459c0f8311af3ff2148bf7d8fd3669ee201f
amanavi75 <amanavi75@gmail.com>
2024-01-26 00:14:12 +0530

    added model for review and fixed some bugs in show.ejs

--- models/listing.js
+++ models/listing.js
@@ -18,6 +18,10 @@ const listingSchema = new Schema({
   price: Number,
   location: String,
   country: String,
+  reviews:[{
+    type:Schema.Types.ObjectId,
+    ref:"Review",
+  }]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
