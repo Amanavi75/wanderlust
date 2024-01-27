24316d88e6a58e5fa5c7a41ded29666dd9a08f38
amanavi75 <amanavi75@gmail.com>
2024-01-27 23:00:57 +0530

    done with the review feature of saving review in the database

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const {listingSchema} = require("./schema.js");
+const Review = require("./models/review.js")
 
 
 const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust"
@@ -110,6 +111,19 @@ app.get("/listings/:id/edit",validateListing, wrapAsync(async (req, res) => {
     res.redirect("/listings");
 
   }));
+
+  //reviews
+  app.post("/listings/:id/reviews",async(req,res)=>{
+    const listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review)
+
+    listing.reviews.push(newReview);
+    
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+  })
 
 
 
