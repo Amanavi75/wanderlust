8a775497585649920f0d78e56e19e2ac9638969f
amanavi75 <amanavi75@gmail.com>
2024-01-27 23:23:53 +0530

    done with the server side validation

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const ejsMate = require("ejs-mate");
 //use to create the template for the the all the page of website
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema,reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js")
 
 
@@ -41,8 +41,20 @@ app.get('/',(req,res)=>{
     res.send("hi i am root ");
 })
 
+// method for validating the listings
 const validateListing = (req,res,next) =>{
   let {error} = listingSchema.validate(req.body);
+    if(error){
+      let errorMsg = error.details.map((el)=> el.message).join(",")
+      throw new ExpressError(400,errorMsg)
+    }else{
+      next();
+    }
+}
+
+//method for validating the review
+const validateReview = (req,res,next) =>{
+  let {error} = reviewSchema.validate(req.body);
     if(error){
       let errorMsg = error.details.map((el)=> el.message).join(",")
       throw new ExpressError(400,errorMsg)
@@ -113,7 +125,7 @@ app.get("/listings/:id/edit",validateListing, wrapAsync(async (req, res) => {
   }));
 
   //reviews
-  app.post("/listings/:id/reviews",async(req,res)=>{
+  app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
     const listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review)
 
@@ -123,7 +135,7 @@ app.get("/listings/:id/edit",validateListing, wrapAsync(async (req, res) => {
     await listing.save();
 
     res.redirect(`/listings/${listing._id}`);
-  })
+  }))
 
 
 
