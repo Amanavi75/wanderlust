d9e9b6ed92518a70e5405c3e90c709454dc525e2
amanavi75 <amanavi75@gmail.com>
2024-01-27 23:54:31 +0530

    done with the rendering of the reviews and its styling

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.get("/listings", async (req, res) => {
   //Show Route 
   app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
-    const listing = await Listing.findById(id)
+    const listing = await Listing.findById(id).populate("reviews")
     res.render("listings/show.ejs", {listing});
   }
   ));
