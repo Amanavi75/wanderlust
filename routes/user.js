03db33ec18ae5f2fec6ccc22ae421d916c341ad3
amanavi75 <amanavi75@gmail.com>
2024-01-29 00:07:22 +0530

    done with login and logout functionality

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express  = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs")
@@ -32,4 +33,15 @@ router.post("/signup",wrapAsync(async(req,res)=>{
 
 })) 
 
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs")
+})
+
+
+router.post("/login",
+passport.authenticate("local", {failureRedirect: '/login',failureFlash:true})
+,async(req,res)=>{
+    req.flash("success","welcome back to wanderlust")
+    res.redirect("/listings");
+})
 module.exports = router;
