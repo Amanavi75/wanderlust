041b731a04b64b4540552221d5d5779b210d4168
amanavi75 <amanavi75@gmail.com>
2024-01-31 23:35:39 +0530

    done with the post login page ..redirectin to that page only where we we started login

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const {saveRedirectUrl} =require("../middleware.js")
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs")
@@ -43,11 +44,12 @@ router.get("/login",(req,res)=>{
 })
 
 
-router.post("/login",
+router.post("/login",saveRedirectUrl,
 passport.authenticate("local", {failureRedirect: '/login',failureFlash:true})
 ,async(req,res)=>{
     req.flash("success","welcome back to wanderlust")
-    res.redirect("/listings");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
 })
 
 
