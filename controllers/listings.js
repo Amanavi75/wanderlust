14ab2cb7f8ada2710cdca7a82ae9fa972e638a46
amanavi75 <amanavi75@gmail.com>
2024-02-06 00:05:00 +0530

    added functionality for the image preview

--- controllers/listings.js
+++ controllers/listings.js
@@ -47,7 +47,9 @@ module.exports.editListing = async (req, res) => {
     req.flash("error","listing doesn't exist");
     res.redirect("/listings")
   }
-  res.render("listings/edit.ejs", { listing });
+  let originalImageUrl= listing.image.url;
+  originalImageUrl=originalImageUrl.replace("/upload","/upload/h_300,w_250")
+  res.render("listings/edit.ejs", { listing ,originalImageUrl});
 }
 
 module.exports.updateListing = async(req,res)=>{
